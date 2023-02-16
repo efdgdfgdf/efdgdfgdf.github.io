3323c24eb5e12f5418fc72427fa60414566422f6
efdgdfgdf <2571599538@qq.com>
2023-02-16 22:07:41 +0800

    Site updated: 2023-02-16 22:07:40

--- js/custom.js
+++ js/custom.js
@@ -198,7 +198,6 @@ var anzhiyu = {
   },
 };
 
-anzhiyu.commentText(e);
 anzhiyu.changeTimeInEssay();
 anzhiyu.reflashEssayWaterFall();
 
@@ -243,4 +242,250 @@ window.onload = function () {
   window.onscroll = function () {
       lazyload(imgs);
   }
+}
+
+
+/**
+ * 切换背景
+ */ 
+// 切换链接对应的背景(加入了链接检验与防抖)
+function getPicture() {
+  debounce(getPicture_, 300);
+}
+
+function getPicture_() {
+  let bg = document.getElementById("web_bg");
+  checkImgExists(document.getElementById("pic-link").value).then(() => {
+    // 有效的图片链接
+    var link = "url(" + document.getElementById("pic-link").value + ")";
+    bg.style.backgroundImage = link;
+    localStorage.setItem("blogbg", link);
+    localStorage.setItem("bing", "false");
+    if (document.getElementById("bingSet")) document.getElementById("bingSet").checked = false;
+    // 提示切换成功
+    new Vue({
+      data: function () {
+        this.$notify({
+          title: "可以啦🍨",
+          message: "切换自定义背景成功！",
+          position: 'top-left',
+          offset: 50,
+          showClose: true,
+          type: "success",
+          duration: 5000
+        });
+      }
+    })
+  }).catch(() => {
+    // 无效的图片链接，提示无效
+    new Vue({
+      data: function () {
+        this.$notify({
+          title: "链接不对🤣",
+          message: "请输入有效的图片链接！",
+          position: 'top-left',
+          offset: 50,
+          showClose: true,
+          type: "warning",
+          duration: 5000
+        });
+      }
+    })
+  })
+}
+// 判断图片链接是否可用
+function checkImgExists(imgurl) {
+  return new Promise(function (resolve, reject) {
+    var ImgObj = new Image();
+    ImgObj.src = imgurl;
+    ImgObj.onload = function (res) {
+      resolve(res);
+    }
+    ImgObj.onerror = function (err) {
+      reject(err);
+    }
+  })
+}
+
+
+// 必应每日图片
+if (localStorage.getItem("bing") == undefined) {
+  localStorage.setItem("bing", "false");
+}
+if (localStorage.getItem("bing") == "true") {
+  let bg = document.getElementById("web_bg");
+  // 手机电脑分开
+  let curUrl = screen.width <= 768 ? "url(https://bing.img.run/m.php)" : "url(https://bing.img.run/1920x1080.php)";
+  bg.style.backgroundImage = curUrl;
+}
+function setBing() {
+  // 打开就设置
+  if (document.getElementById("bingSet").checked) {
+    let bg = document.getElementById("web_bg");
+    // 手机电脑分开
+    let curUrl = screen.width <= 768 ? "url(https://bing.img.run/m.php)" : "url(https://bing.img.run/1920x1080.php)";
+    bg.style.backgroundImage = curUrl;
+    localStorage.setItem("bing", "true");
+    localStorage.removeItem("blogbg");
+  } else {
+    // 关闭就移除并恢复默认壁纸
+    localStorage.setItem("bing", "false");
+    setTimeout(reload, 600);
+  }
+}
+
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+  localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+  let d = JSON.parse(localStorage.getItem(name));
+  // 过期或有错误返回 0 否则返回数据
+  if (d) {
+      let t = Date.now() - d.time
+      if (t < (time * 60 * 1000) && t > -1) return d.data;
+  }
+  return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+  let data = loadData('blogbg', 1440)
+  if (data) changeBg(data, 1)
+  else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+  let bg = document.getElementById('web_bg')
+  if (s.charAt(0) == '#') {
+      bg.style.backgroundColor = s
+      bg.style.backgroundImage = 'none'
+  } else bg.style.backgroundImage = s
+  if (!flag) { saveData('blogbg', s) }
+}
+
+// 以下为2.0新增内容
+
+// 创建窗口
+var winbox = ''
+
+function createWinbox() {
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  winbox = WinBox({
+      id: 'changeBgBox',
+      index: 999,
+      title: "⚙️设置背景",
+      x: "left",
+      y: "center",
+      minwidth: '300px',
+      height: "60%",
+      background: '#5fcdff',
+      onmaximize: () => { div.innerHTML = `<style>body::-webkit-scrollbar {display: none;}div#changeBgBox {width: 100% !important;}</style>` },
+      onrestore: () => { div.innerHTML = '' }
+  });
+  winResize();
+  window.addEventListener('resize', winResize)
+
+  // 每一类我放了一个演示，直接往下复制粘贴 a标签 就可以，需要注意的是 函数里面的链接 冒号前面需要添加反斜杠\进行转义
+  winbox.body.innerHTML = `
+  <div class="settings" style="display: block;">
+  <div id="article-container" style="padding:12px;">
+  <br>
+	<center>
+    <p>
+      <button onclick="localStorage.removeItem('blogbg');location.reload();" style="background:linear-gradient(-225deg, #7DE2FC 0%, #B9B6E5 100%);display:block;width:40%;padding:15px 0;border-radius:30px;color:white;font-size:1.1em;">
+        <i class="fa-solid fa-arrows-rotate"></i> 点我恢复默认背景
+      </button>
+    </p>
+	</center>
+
+  <h2>1. 二次元</h2>
+  <details class="folding-tag" cyan><summary> 查看二次元背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://sourcebucket.s3.ladydaily.com/img/home_bg.webp)" class="imgbox" onclick="changeBg('url(https\://sourcebucket.s3.ladydaily.com/img/home_bg.webp)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://www.dmoe.cc/random.php)" class="imgbox" onclick="changeBg('url(https://www.dmoe.cc/random.php)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://api.ixiaowai.cn/api/api.php)" class="imgbox" onclick="changeBg('url(https://api.ixiaowai.cn/api/api.php)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://api.mtyqx.cn/tapi/random.php)" class="imgbox" onclick="changeBg('url(https://api.mtyqx.cn/tapi/random.php)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://cdn.seovx.com/d/?mom=302" class="imgbox" onclick="changeBg('url(https://cdn.seovx.com/d/?mom=302)')"></a></div>
+              </div>
+            </details>
+
+  <h2>2. 风景</h2>
+  <details class="folding-tag" cyan><summary> 查看风景背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://sourcebucket.s3.ladydaily.com/img/fj1.webp)" class="imgbox" onclick="changeBg('url(https://sourcebucket.s3.ladydaily.com/img/fj1.webp)')"></a></div>
+              </div>
+            </details>
+
+  <h2>3. 萌宠</h2>
+  <details class="folding-tag" cyan><summary> 查看萌宠背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://sourcebucket.s3.ladydaily.com/img/mc1.webp)" class="imgbox" onclick="changeBg('url(https://sourcebucket.s3.ladydaily.com/img/mc1.webp)')"></a></div>
+              </div>
+            </details>
+
+  <h2>4. 渐变色</h2>
+  <details class="folding-tag" cyan><summary> 查看渐变色背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to top, #355c7d, #6c5b7b, #c06c84)" onclick="changeBg('linear-gradient(to top, #355c7d, #6c5b7b, #c06c84)')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #eecda3, #ef629f)" onclick="changeBg('linear-gradient(to right, #eecda3, #ef629f)')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(90deg,rgba(247,149,51,.1) 0,rgba(243,112,85,.1) 15%,rgba(239,78,123,.1) 30%,rgba(161,102,171,.1) 44%,rgba(80,115,184,.1) 58%,rgba(16,152,173,.1) 72%,rgba(7,179,155,.1) 86%,rgba(109,186,130,.1) 100%)" onclick="changeBg('linear-gradient(90deg,rgba(247,149,51,.1) 0,rgba(243,112,85,.1) 15%,rgba(239,78,123,.1) 30%,rgba(161,102,171,.1) 44%,rgba(80,115,184,.1) 58%,rgba(16,152,173,.1) 72%,rgba(7,179,155,.1) 86%,rgba(109,186,130,.1) 100%)')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #005f58, #00205a)" onclick="changeBg('linear-gradient(to right, #005f58, #00205a)')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(-225deg, #B7F8DB 0%, #50A7C2 100%)" onclick="changeBg('linear-gradient(-225deg, #B7F8DB 0%, #50A7C2 100%)')"></a></div>
+              </div>
+            </details>
+
+  <h2>5. 纯色</h2>
+  <details class="folding-tag" cyan><summary> 查看纯色背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #7D9D9C" onclick="changeBg('#7D9D9C')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #000000ab" onclick="changeBg('#000000ab')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #ededed" onclick="changeBg('#ededed')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #00aac3" onclick="changeBg('#00aac3')"></a><a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #f7eff5" onclick="changeBg('#f7eff5')"></a>  <input type="color" id="colors" href="javascript:;" rel="noopener external nofollow" class="box" autocomplete="on" value="#6dd5ed" oninput="changeBgColor()"></input></div>
+              </div>
+            </details>
+
+  <h2>6. 适配手机</h2>
+  <details class="folding-tag" cyan><summary> 查看适配手机的背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://img.xjh.me/random_img.php?return=302)" class="pimgbox" onclick="changeBg('url(https://img.xjh.me/random_img.php?return=302)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://sourcebucket.s3.ladydaily.com/img/mb4.webp)" class="pimgbox" onclick="changeBg('url(https\://sourcebucket.s3.ladydaily.com/img/mb4.webp)')"></a>
+              </div>
+            </details>
+
+  <h2>7. 随机壁纸API</h2>
+  <details class="folding-tag" cyan><summary> 查看热门壁纸API系列背景 </summary>
+              <div class='content'>
+              <div class="bgbox"><a id="bingDayBox" rel="noopener external nofollow" style="background-image:url(https://bing.img.run/m.php)" class="box apiBox" onclick="changeBg('url(https://bing.img.run/m.php)')"></a><a id="bingHistoryBox" rel="noopener external nofollow" style="background-image:url(https://bing.img.run/rand_m.php)" class="box apiBox" onclick="changeBg('url(https://bing.img.run/rand_m.php)')"></a><a id="EEEDogBox" rel="noopener external nofollow" style="background-image:url(https://api.yimian.xyz/img?type=moe&size=1920x1080)" class="box apiBox" onclick="changeBg('url(https://api.yimian.xyz/img?type=moe&size=1920x1080)')"></a><a id="seovxBox" rel="noopener external nofollow" style="background-image:url(https://cdn.seovx.com/?mom=302)" class="box apiBox" onclick="changeBg('url(https://cdn.seovx.com/?mom=302)')"></a><a id="picsumBox" rel="noopener external nofollow" style="background-image:url(https://picsum.photos/1920/1080.webp)" class="box apiBox" onclick="changeBg('url(https://picsum.photos/1920/1080.webp)')"></a><a id="waiBizhiBox" rel="noopener external nofollow" style="background-image:url(https://api.ixiaowai.cn/gqapi/gqapi.php)" class="box apiBox" onclick="changeBg('url(https://api.ixiaowai.cn/gqapi/gqapi.php)')"></a><a id="btstuBox" rel="noopener external nofollow" style="background-image:url(https://api.btstu.cn/sjbz/api.php)" class="box apiBox" onclick="changeBg('url(https://api.btstu.cn/sjbz/api.php)')"></a><a id="unsplashBox" rel="noopener external nofollow" style="background-image:url(https://source.unsplash.com/random/1920x1080/)" class="box apiBox" onclick="changeBg('url(https://source.unsplash.com/random/1920x1080/)')"></a><a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://api.aagtool.top/api/sjtp?type=json&image=all)" class="box apiBox" onclick="changeBg('url(https://api.aagtool.top/api/sjtp?type=json&image=all)')"></a>
+              </div>
+            </details>
+
+  <h2>8. 自定义背景</h2>
+  <details class="folding-tag" cyan><summary> 设置自定义背景 </summary>
+              <div class='content'>
+              <p><center><input type="text" id="pic-link" size="70%" maxlength="1000" placeholder="请输入有效的图片链接，如 https://tuchuang.voooe.cn/images/2023/02/15/city_in_the_clouds_by_tatasz_d8yebbu.png"></center></p><p><center><button type="button" onclick="getPicture()" style="background:#5fcdff;width:35%;padding: 5px 0px 7px 0px;border-radius:30px;color:white;line-height:2;">🥏切换背景🥏</button></center></p>
+              </div>
+            </details>
+
+  <br>
+  <center><div style="font-size:1.2em;color:#5fcdff;font-weight:bold;">------ ( •̀ ω •́ )y 到底啦 ------</div></center>
+  <br>
+
+  </div>
+  </div>
+`;
+}
+
+// 适应窗口大小
+function winResize() {
+  var offsetWid = document.documentElement.clientWidth;
+  if (offsetWid <= 768) {
+      winbox.resize(offsetWid * 0.95 + "px", "90%").move("center", "center");
+  } else {
+      winbox.resize(offsetWid * 0.6 + "px", "70%").move("center", "center");
+  }
+}
+
+// 切换状态，窗口已创建则控制窗口显示和隐藏，没窗口则创建窗口
+function toggleWinbox() {
+  if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
+  else createWinbox();
 }
---
+++ ldp/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/f1a5/","posts/1342/","posts/0000/","posts/1231/","posts/2892/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
