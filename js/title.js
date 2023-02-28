31650f129ad21e776b9c867d6bc48eb53c762631
efdgdfgdf <2571599538@qq.com>
2023-02-28 14:53:30 +0800

    Site updated: 2023-02-28 14:53:30

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '👻别走嘛，坐下来喝茶鸭☕';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '🐷死鬼！你总算回来了~😼';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="👻别走嘛，坐下来喝茶鸭☕",clearTimeout(titleTime)):(document.title="🐷死鬼！你总算回来了~😼",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
